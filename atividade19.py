7f5c94f9f586faddec4e28a121aa001dc9626fd1
Kaliandrik <contatokaliandrik@gmail.com>
2024-09-19 11:02:48 -0300

    Calcular fatorial

--- atividade19.py
+++ atividade19.py
@@ -1,13 +1,16 @@
 # Crie um programa que receba um número inteiro e calcule o fatorial desse número usando uma estrutura de repetição.
 
 
+
+
 numero = int(input("Digite o número: "))
 
+
 resultado = 1
+
 
 for n in range(1, numero +1):
     resultado = resultado * n
 
-print('O resultado de {0}! é: {1}'.format(numero, resultado))
 
-
+print(f'O resultado de {numero}! é {resultado}')
